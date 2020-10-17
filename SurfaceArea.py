ccc70f64529a30e04b62258995ad65674ebdc336
Suhail-Athar <Suhail-Athar@users.noreply.github.com>
2020-10-18 05:25:33 +0530

    Create SurfaceArea.py
    To calculate surface area of cylinder using user's input.

---
+++ SurfaceArea.py
@@ -0,0 +1,10 @@
+#To Calculate Surface area of a cylinder
+#Formula = (2*3.14*r*h)+( 2*3.14*r*r)
+
+r=float(input('Enter the radius of the Cylinder:  '))
+
+h=float(input('Enter the height of the Cylinder:  '))
+
+SA= (2*3.14*r*h) + ( 2*3.14*r*r)
+
+print('The Surface Area is:', SA,' sq. m')
