e8f8196e3d4446eaed938681808ed3783884863b
Suhail-Athar <Suhail-Athar@users.noreply.github.com>
2020-10-18 23:30:44 +0530

    Snowman with Redhat

--- Turtle/SnowMan.py
+++ Turtle/SnowMan.py
@@ -1,95 +1,82 @@
 # Import required module 
-import turtle 
-
-
-
-# Create turtle object 
-t = turtle.Turtle() 
-
-# Create a screen 
-screen =turtle.Screen() 
+import turtle as hail
 
 # Set background color 
-screen.bgcolor("sky blue") 
-
-
+hail.bgcolor("#4863A0")
 
 # Function to draw body of snowman 
 def draw_circle(color, radius, x, y): 
-	t.penup() 
-	t.fillcolor (color) 
-	t.goto (x, y) 
-	t.pendown() 
-	t.begin_fill() 
-	t.circle (radius) 
-	t.end_fill() 
-
-
+	hail.penup()
+	hail.width(3)
+	hail.fillcolor (color) 
+	hail.goto (x, y) 
+	hail.pendown() 
+	hail.begin_fill() 
+	hail.circle (radius) 
+	hail.end_fill() 
 	
-# Illustrating snowman 
-# Drawing snowman body 
-draw_circle ("#ffffff", 30, 0, -40) 
-draw_circle ("#ffffff", 40, 0, -100) 
-draw_circle ("#ffffff", 60, 0, -200) 
+#Drawing snowman body 
+draw_circle ("#ffffff", 60, 0, 40) 
+draw_circle ("#ffffff", 90, 0, -100) 
+draw_circle ("#ffffff", 120, 0, -270) 
 
 # Drawing left eye 
-draw_circle ("#ffffff", 2, -10, -10) 
-
-# Drawing right eye 
-draw_circle ("#ffffff", 2, 10, -10) 
+draw_circle ("#ffffff", 8, -22, 110) 
+draw_circle ("#ffffff", 8, 22, 110)
 
 # Drawing nose 
-draw_circle ("#FF6600", 3, 0, -15) 
+draw_circle ("#FF6600", 6, 0, 90)
 
-# Drawing buttons on 
-draw_circle ("#ffffff", 2, 0, -35) 
-draw_circle ("#ffffff", 2, 0, -45) 
-draw_circle ("#ffffff", 2, 0, -55) 
-
-
+# Body Buttons 
+draw_circle ("#ffffff", 4, 0, 45) 
+draw_circle ("#ffffff", 4, 0, 10) 
+draw_circle ("#ffffff", 4, 0, -23)
 
 # Function to draw arms 
 def create_line(x, y, length, angle): 
-	t.penup() 
-	t.goto(x, y) 
-	t.setheading(angle) 
-	t.pendown() 
-	t.forward(length) 
-	t.setheading(angle + 20) 
-	t.forward(20) 
-	t.penup() 
-	t.back(20) 
-	t.pendown() 
-	t.setheading(angle - 20) 
-	t.forward(20) 
-	t.penup() 
-	t.home() 
-	
+	hail.penup()
+	hail.width(11)
+	hail.goto(x, y) 
+	hail.setheading(angle) 
+	hail.pendown() 
+	hail.forward(length) 
+	hail.setheading(angle + 21) 
+	hail.forward(40) 
+	hail.penup() 
+	hail.back(40) 
+	hail.pendown() 
+	hail.setheading(angle - 21) 
+	hail.forward(40) 
+	hail.penup() 
+	hail.home() 
 
-	
-# Drawing left arm 
-create_line(-70, -50, 50, 160) 
+#Arms
+create_line(-80, 0, 75, 160)
+create_line(80, 0, 75, 20)
 
-# Drawing right arm 
-create_line(70, -50, 50, 20) 
-
-
+#Legs
+create_line(-80, -200, 75, 210)
+create_line(80, -200, 75, -30)
 
 # Drawing hat 
-t.penup() 
-t.goto (-35, 8) 
-t.color ("black") 
-t.pensize (6) 
-t.pendown() 
-t.goto (35, 8) 
+hail.penup() 
+hail.goto (-40, 140) 
+hail.color ("black") 
+hail.width(5) 
+hail.pendown() 
+hail.goto (45, 140)
+#hail.goto (45, 140) 
+hail.fillcolor ("#9F000F") 
+hail.begin_fill() 
+hail.left (90) 
+hail.forward (25) 
+hail.left (90) 
+hail.forward (95) 
+hail.left (90) 
+hail.forward (25)
+hail.left (90)
+hail.forward (85)
+hail.end_fill()
+hail.ht()
 
-t.goto (30, 8) 
-t.fillcolor ("black") 
-t.begin_fill() 
-t.left (90) 
-t.forward (15) 
-t.left (90) 
-t.forward (60) 
-t.left (90) 
-t.forward (15) 
-t.end_fill()
+hail.done()
