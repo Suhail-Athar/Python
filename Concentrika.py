342e2e295ce030e6a98bc32606e70980021a8cdf
Suhail-Athar <Suhail-Athar@users.noreply.github.com>
2020-10-18 06:33:24 +0530

    Create colorful random circles.
    Python program to create concentric circles of random colors.

--- Concentrika.py
+++ Concentrika.py
@@ -1,12 +1,12 @@
 import turtle    # importing the module
 import random    # importing random module for generating random numbers
-trtl = turtle.Turtle()    #making a turtle object of Turtle class for drawing
+hail = turtle.Turtle()    #making a turtle object of Turtle class for drawing
 screen=turtle.Screen()    #making a canvas for drawing
 screen.setup(420,320)    #choosing the screen size
 screen.bgcolor('lavender')    #making canvas black
-trtl.pensize(10)    #choosing the size of pen nib
-trtl.speed(1)    #choosing the speed of drawing
-trtl.shape('turtle')   #choosing the shape of pen nib
+hail.pensize(10)    #choosing the size of pen nib
+hail.speed(1)    #choosing the speed of drawing
+hail.shape('turtle')   #choosing the shape of pen nib
 
 n=0
 while n<14:
@@ -14,10 +14,10 @@ while n<14:
     g=random.randint(81,200)   #random numbers for green
     b=random.randint(61,255)   #random numbers for blue
     turtle.colormode(255)   #declaring the colour mode
-    trtl.pencolor(r,g,b)   #pen colour
+    hail.pencolor(r,g,b)   #pen colour
     n=n+1
-    trtl.penup()
-    trtl.setpos(0,-n*20)
-    trtl.pendown()
-    trtl.circle(20*n)
-trtl.mainloop()
+    hail.penup()
+    hail.setpos(0,-n*20)
+    hail.pendown()
+    hail.circle(20*n)
+hail.mainloop()
