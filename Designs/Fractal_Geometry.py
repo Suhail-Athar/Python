74f0780b6294f1cc34b3d7441c4aa257c4707318
Mohd Suhail Athar <su.hail@live.in>
2020-11-06 04:35:08 +0530

    Update Fractal_Geometry.py

--- Designs/Fractal_Geometry.py
+++ Designs/Fractal_Geometry.py
@@ -7,20 +7,20 @@ hail.color('grey')
 style = ('verdana', 30, 'bold')
 hail.pu()
 hail.goto (0,240)
-hail.write('FRACTAL GEOMETRY', font=style, align='center')
+hail.write('FRACTAL GEOMETRY', font=style, align='center') #Heading
 hail.goto(0,-50)
 hail.pd()
 
-colors  = ["#f4d078","#ec6262","#bdd8d8","#9c789b","#8aa8bf","#61814e","#92c374"] # List of colors
+colors  = ["#f4d078","#ec6262","#bdd8d8","#9c789b","#8aa8bf","#61814e","#92c374"]  #Colors list
 hail.width(14)
 length = 5
 hail.speed(3)
 
 for count in range(110):
-  color = random.choice(colors) #Choose a random color
+  color = random.choice(colors) 
   hail.forward(length)
   hail.right(135)
-  hail.color(color) # Why is color spelt like this?
+  hail.color(color)
   length = length + 5
 
 hail.mainloop()
